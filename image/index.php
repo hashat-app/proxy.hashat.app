d45a8337811c73bb2bc9dadbf78fa6ff9348104d
Kurt Krüger <KurtK06@outlook.de>
2023-08-16 18:16:53 +0200

    Speed optimization (WebP) and resizing support

--- image/index.php
+++ image/index.php
@@ -1,19 +1,31 @@
 <?php
 
 // Check that the proxy is only used on the domain "hashat.app"
-// if (!preg_match('/^https?:\/\/hashat\.app/', $_SERVER['HTTP_REFERER'])) {
-//     http_response_code(403);
-//     echo 'Error: This proxy can only be used on the domain "hashat.app"';
-//     exit;
-// }
+// ! [IMPORTANT] Change the domain to your own domain or remove the if statement if you want to use the proxy on all domains
+if ($_SERVER['HTTP_HOST'] !== 'hashat.app') {
+    // Display error image because the proxy is not used on the domain "hashat.app"
+    header('Content-Type: image/webp');
+    $image = imagecreatefromwebp('error-4.webp');
+    imagewebp($image);
+    imagedestroy($image);
+    exit;
+}
 
 // The image URL to proxy
-$imageUrl = $_GET['url'];
+@$imageUrl = $_GET['url'];
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 
 // Check if the URL is valid
 if (filter_var($imageUrl, FILTER_VALIDATE_URL) === false OR empty($imageUrl)) {
-    // display error image
-    header('Location: https://proxy.hashat.app/image/error-1.png');
+    // Display error image because the URL is invalid
+    header('Content-Type: image/webp');
+    $image = imagecreatefromwebp('error-1.webp');
+    imagewebp($image);
+    imagedestroy($image);
     exit;
 }
 
@@ -22,7 +34,9 @@ $ch = curl_init($imageUrl);
 
 // Set the HTTP headers from the request
 curl_setopt($ch, CURLOPT_HTTPHEADER, [
-    'User-Agent: ' . $_SERVER['HTTP_USER_AGENT']
+    // Send custom user agent for Hashat Bot
+    // ! [IMPORTANT] Change the user agent to your own user agent
+    'User-Agent: Mozilla/5.0 (compatible; HashatBot/2.0; +https://hashat.app);'
 ]);
 
 // Return the response as a string
@@ -36,21 +50,24 @@ $imageData = curl_exec($ch);
 
 // Check if the request timed out
 if (curl_errno($ch) == CURLE_OPERATION_TIMEDOUT) {
-    // Display the error image
-    header('Content-Type: image/png');
-    $image = imagecreatefrompng('error-1.png');
-    imagepng($image);
+    // Display the error image because the request timed out
+    header('Content-Type: image/webp');
+    $image = imagecreatefromwebp('error-1.webp');
+    imagewebp($image);
     imagedestroy($image);
     exit;
 }
 
+// Close the cURL session
+curl_close($ch);
+
 // Check if the file is an image
 $finfo = finfo_open(FILEINFO_MIME_TYPE);
-$mimeType = finfo_buffer($finfo, $imageData);
-if (strpos($mimeType, 'image/') === 0) {
-    // Set the content type to the content type of the image
-    $contentType = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
-    header('Content-Type: ' . $contentType);
+$mime_type = finfo_buffer($finfo, $imageData);
+finfo_close($finfo);
+if (strpos($mime_type, 'image/') === 0) {
+    // send WebP header
+    header('Content-Type: image/webp');
 
     if (isset($_GET['ratio'])) {
         // Get the desired aspect ratio
@@ -58,8 +75,8 @@ if (strpos($mimeType, 'image/') === 0) {
         $ratio_parts = explode(':', $ratio);
         if (count($ratio_parts) != 2) {
             // display error image
-            header('Content-Type: image/png');
-            $image = imagecreatefrompng('error-2.png');
+            header('Content-Type: image/webp');
+            $image = imagecreatefromwebp('error-2.webp');
             imagepng($image);
             imagedestroy($image);
             exit;
@@ -67,7 +84,7 @@ if (strpos($mimeType, 'image/') === 0) {
         $ratio_width = $ratio_parts[0];
         $ratio_height = $ratio_parts[1];
 
-        if ($mimeType == 'image/jpeg') {
+        if ($mime_type == 'image/jpeg') {
             // Load the JPEG image
             $image = imagecreatefromjpeg($imageUrl);
         } else {
@@ -93,29 +110,66 @@ if (strpos($mimeType, 'image/') === 0) {
         }
 
         // Output the cropped image
-        if ($mimeType == 'image/jpeg') {
-            imagejpeg($cropped_image);
+        if ($mime_type == 'image/jpeg') {
+            // generate webp image from jpeg
+            imagewebp($cropped_image);
         } else {
-            imagepng($cropped_image);
+            // generate webp image from png
+            imagewebp($cropped_image);
         }
 
         // Clean up
         imagedestroy($image);
         imagedestroy($cropped_image);
+    } elseif (isset($_GET['size'])) {
+        // Get the desired size
+        $size = $_GET['size'];
+        $size_parts = explode('x', $size);
+        if (count($size_parts) !== 2) {
+            // Display error image because there are to less or to many size parts
+            header('Content-Type: image/webp');
+            $image = imagecreatefromwebp('error-2.webp');
+            imagepng($image);
+            imagedestroy($image);
+            exit;
+        }
+        $width = $size_parts[0];
+        $height = $size_parts[1];
+
+        if ($mime_type == 'image/jpeg') {
+            // Load the JPEG image
+            $image = imagecreatefromjpeg($imageUrl);
+        } else {
+            // Load the image
+            $image = imagecreatefromstring($imageData);
+        }
+
+        // Get the current width and height of the image
+        $current_width = imagesx($image);
+        $current_height = imagesy($image);
+
+        // Calculate the new height and width based on the desired size
+        $new_width = $width;
+        $new_height = $height;
+
+        // Resize the image
+        $resized_image = imagescale($image, $new_width, $new_height);
+
+        // Output the resized image
+        imagewebp($resized_image);
+
+        // Clean up
+        imagedestroy($image);
+        imagedestroy($resized_image);
     } else {
         // Output the image data
         echo $imageData;
     }
 } else {
-    // display error image
-    header('Content-Type: image/png');
-    $image = imagecreatefrompng('error-3.png');
-    imagepng($image);
+    // Display error image because the file is not an image
+    header('Content-Type: image/webp');
+    $image = imagecreatefromwebp('error-3.webp');
+    imagewebp($image);
     imagedestroy($image);
     exit;
 }
-
-finfo_close($finfo);
-
-// Close the cURL session
-curl_close($ch);
